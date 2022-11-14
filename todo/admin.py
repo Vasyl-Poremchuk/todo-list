3eba3e595544613d80fbda8719d5d3fc08084aaa
Vasyl Poremchuk <nleveryone@gmail.com>
2022-11-14 13:47:38 +0200

    Create migrations

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Tag, Task
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("content", "create")
+    list_filter = ("content", "create")
